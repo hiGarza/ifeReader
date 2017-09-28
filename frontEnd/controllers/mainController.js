2df772cd5698827277aa4668769950e1bc5b856e
Humberto Garza <humberto@ooyala.com>
2017-09-27 20:58:21 -0500

    Post access data to Dynamo

--- frontEnd/controllers/mainController.js
+++ frontEnd/controllers/mainController.js
@@ -1,20 +1,21 @@
 eqApp.controller('mainController', function($rootScope, $scope, $http) {
 	$scope.person = {image:"placeholder.jpg"};
-	console.log($scope.person.userID);
     var evtSource = new EventSource("//127.0.0.1:8080");
     evtSource.onmessage = function(e) {
 		$scope.person = JSON.parse(e.data);
 		var date = "";
 		$scope.person.date = new Date($scope.person.access);
-		console.log($scope.person.date);
-		console.log(typeof $scope.person.date);
 		$scope.person.image = $scope.person.userID+".jpg"
 		$scope.$apply();
 	}
 
 	$scope.save = function(){
 		if($scope.person.userID !== undefined){
-			$scope.person = {image:"placeholder.jpg"};
+			$http.post('https://mwalm5619b.execute-api.us-west-1.amazonaws.com/production/access', $scope.person)
+			.then(function(response){
+	        	console.log(response);
+	        	$scope.person = {image:"placeholder.jpg"};
+	      	}, function(error){console.log(error)});
 		}
 	};
 });
